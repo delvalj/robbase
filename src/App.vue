c599a029c1eea817082419a7d48cc728098f3a05
Joaquin <delvaljoaquin@gmail.com>
2021-07-22 17:35:44 -0300

    se hace o ke

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
 <p> PEPEPEPEPEPs</p>
+<p> 45</p>
+<p> || </p>
+<p> sa</p>
+<p> PEPEPEPEPEPs</p>
+<p> PEPEPEPEPEPs</p>
+<p> PEPEPEPEPEPs</p>
+<p> PEPEPEPEPEPs</p>
 </template>
 
 <script>
