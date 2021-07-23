f2d9577431f0f30cded8660359502e8f21b11e45
Joaquin <delvaljoaquin@gmail.com>
2021-07-23 15:59:36 -0300

    agrego estilos

--- src/App.vue
+++ src/App.vue
@@ -11,3 +11,32 @@
 <script>
 export default {};
 </script>
+
+<style>
+*{
+  box-sizing: border-box;
+}
+html{}
+body {
+  margin: 0;
+  padding: 0;
+}
+section{
+  height: 100vh;
+  width: 100%;
+  display: flex;
+
+}
+.choice{
+  width: 50%;
+  margin: auto;
+  text-align: center;
+  padding: 40px;
+  border: 3px solid white;
+}
+
+.question{
+  color: white;
+  font-size: 50px;
+}
+</style>
