decb94d6245cf8f478e236fbec6a7b0e880109a9
Joaquin <delvaljoaquin@gmail.com>
2021-07-24 12:37:44 -0300

    agrego pantallas

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,47 @@
 <template>
-  <section style="background-color: red">
+  <div class="container">
+  <section id="1" style="background-color: red">
     <div class="choice">
-      <div class="question">ARE YOU ROB STARK?</div>
+      <div class="question">ARE YOU FRANK ZAPPA?</div>
+      <a href="#2">YES</a>
+      <a href="#">NO</a>
+    </div>
+  </section>
+
+  <section id="2" style="background-color: black">
+    <div class="choice">
+      <div class="question">ARE YOU FRANK ZAPPA?</div>
       <a href="">YES</a>
       <a href="">NO</a>
     </div>
   </section>
+
+  <section style="background-color: yellow">
+    <div class="choice">
+      <div class="question">ARE YOU FRANK ZAPPA?</div>
+      <a href="">YES</a>
+      <a href="">NO</a>
+    </div>
+  </section>
+
+  <section style="background-color: green">
+    <div class="choice">
+      <div class="question">ARE YOU FRANK ZAPPA?</div>
+      <a href="">YES</a>
+      <a href="">NO</a>
+    </div>
+  </section>
+
+  </div>
 </template>
 
 <script>
+
 export default {};
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Zen+Tokyo+Zoo&display=swap');
 *{
   box-sizing: border-box;
 }
@@ -38,5 +67,21 @@ section{
 .question{
   color: white;
   font-size: 50px;
+  font-family: "Zen Tokyo Zoo", cursive;
+  margin-bottom: 20px;
+}
+
+.choice a  {
+  color: black;
+  font-size: 20px;
+  padding-right: 20px;
+  font-family: "Zen Tokyo Zoo",serif;
+  background-color: whitesmoke;
+  margin: 5px;
+  text-align: center;
+  padding: 10px;
+  text-decoration: none;
+  border-radius: 4px;
+
 }
 </style>
